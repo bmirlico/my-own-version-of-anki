6bcc3b9d25707a0cb74e2223fefb4666913d466b
bmirlico <bastienmirlicourtois@gmail.com>
2025-11-28 08:18:58 +0100

    added overlay grey with transparency on modal component

--- frontend/src/components/Modal.tsx
+++ frontend/src/components/Modal.tsx
@@ -107,7 +107,7 @@ function Modal({ isOpen, onClose, title, children, size = 'md' }: ModalProps) {
   return (
     // Backdrop (overlay sombre)
     <div
-      className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50 p-4"
+      className="fixed inset-0 z-50 flex items-center justify-center bg-gray-900/30 p-4"
       onClick={handleBackdropClick}
     >
       {/* Contenu du modal */}
